2e96a0f0546db9aecee9dafc63112006bd839bf8
Jorge Felipe <silva.felipe12@hotmail.com>
2025-06-22 21:35:35 -0300

    Update script.js

--- script.js
+++ script.js
@@ -31,19 +31,6 @@ function calcular() {
     document.getElementById('xpFaltante').innerText = `XP restante até Paragon 300: ${xpFaltante.toLocaleString('pt-BR')} XP`;
     document.getElementById('pitsNecessarios').innerText = `Pits necessários: ${pitsNecessarios}`;
     document.getElementById('tempoTotal').innerText = `Tempo total estimado: ${horas} horas e ${minutos} minutos`;
-    function abrirInfo() {
-    document.getElementById('infoModal').style.display = "block";
-}
-
-function fecharInfo() {
-    document.getElementById('infoModal').style.display = "none";
-}
-
-
-window.onclick = function(event) {
-    const modal = document.getElementById('infoModal');
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+    
 
 }
